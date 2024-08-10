adfaa3a26a23a39a67f5296a8ff72f2ad063eb09
Diego santos <diego.s.d.santos07@gmail.com>
2024-08-10 01:29:02 -0300

    fix: Adding create and get all functionalities for category

---
+++ ParkingSystem.Domain/Interfaces/IPriceTableRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace ParkingSystem.Domain.Interfaces;
+
+public interface IPriceTableRepository
+{
+    Task<IEnumerable<PriceTable>> GetAllAsync();
+    Task<PriceTable> GetByIdAsync(int id);
+    Task<PriceTable> CreateAsync(PriceTable priceTable);
+    Task<PriceTable> UpdateAsync(PriceTable priceTable);
+    Task<PriceTable> DeleteAsync(PriceTable priceTable);
+}
